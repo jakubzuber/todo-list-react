824c343243980c9ef17729595a9c91e6b2b66711
Jakub Zuber <jakub.zuber19@gmail.com>
2022-09-26 15:37:09 +0200

    removeTask added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Header from "./Header";
 import Main from "./Main";
 import { useState } from "react";
 
-const tasks = [
-  { id: 1, content: "przejść na react'a", done: true },
-  { id: 2, content: "zrobić placki", done: false },
-];
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "przejść na react'a", done: true },
+    { id: 2, content: "zrobić placki", done: false },
+  ]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone)
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
   return (
@@ -24,12 +27,17 @@ function App() {
       <Section title="Dodaj nowe zadanie" body={<Form />} />
       <Section title="Lista zadań"
         headerButton={
-        <Buttons 
-        tasks={tasks} 
-        hideDone={hideDone} 
-        toggleHideDone={toggleHideDone} 
-        />}
-        body={<Tasks tasks={tasks} hideDone={hideDone} />}
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />}
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+          />}
       />
     </Main>
   );
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,12 +1,17 @@
 import "./style.css"
 
-const Tasks = (props) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
     <ul className="taskList">
-        {props.tasks.map(task => (
-            <li key={task.id} className={`taskList__task ${props.hideDone && task.done ? "taskList__task--hide" : ""}`}>
+        {tasks.map(task => (
+            <li key={task.id} className={`taskList__task ${hideDone && task.done ? "taskList__task--hide" : ""}`}>
                 <button className="list__buttonDone">{task.done ? "✔" : ""}</button>
                 <span className={task.done ? "list_taskText--done" : ""}>{task.content}</span>
-                <button className="list_buttonRemove">🗑</button>
+                <button
+                    onClick={() => removeTask(task.id)}
+                    className="list_buttonRemove"
+                >
+                    🗑
+                </button>
             </li>
         ))}
     </ul>
