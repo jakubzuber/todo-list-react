2c3807f6cca7458e01089200d7d983271ce95ce3
Jakub Zuber <jakub.zuber19@gmail.com>
2022-10-16 22:15:44 +0200

    tasks id

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -13,12 +13,12 @@ const tasksSlice = createSlice({
         toggleHideDone: state => {
             state.hideDone = !state.hideDone
         },
-        toggleTaskDone: ({tasks}, {payload}) => {
-            const index = tasks.findIndex(task => task.id === payload)
+        toggleTaskDone: ({tasks}, {payload: taskId}) => {
+            const index = tasks.findIndex (({ id }) => id === taskId)
             tasks[index].done = !tasks[index].done
         },
-        removeTask: ({tasks}, {payload}) => {
-            const index = tasks.findIndex(task => task.id === payload)
+        removeTask: ({tasks}, {payload: taskId}) => {
+            const index = tasks.findIndex(({ id }) => id === taskId)
             tasks.splice(index, 1)
         },
         setAllDone: ({tasks}) => {
