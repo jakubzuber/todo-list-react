283b4f6d10012dc8fda6dd9ece3312cb1bdc749c
Jakub Zuber <jakub.zuber19@gmail.com>
2022-11-21 19:18:58 +0100

    modal added to example tasks

--- src/features/TasksPage/TasksList/ExampleTasksButton/index.js
+++ src/features/TasksPage/TasksList/ExampleTasksButton/index.js
@@ -1,70 +1,51 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchExapleTasks, selectLoading } from "../../tasksSlice";
 import { Button } from "../../styled";
-//import { ModalButton, ModalText, ModalButtonsGrid, ModalForm } from "./styled";
+import { ModalButton, ModalText, ModalButtonsGrid, ModalForm } from "./styled";
 
 const ExampleTasksButton = () => {
     const dispatch = useDispatch();
     const loading = useSelector(selectLoading);
-    //const [modalIsOpen, setIsOpen] = useState(false);
+    const [modalIsOpen, setIsOpen] = useState(false);
 
-    /*const openModal = () => {
+    const openModal = () => {
         setIsOpen(true);
-    }*/
+    }
 
-   /* const closeModal = () => {
+    const closeModal = () => {
         setIsOpen(false);
-    } */
+    }
 
-    /*const loadingTasks = () => {
+    const loadingTasks = () => {
         setIsOpen(false);
         dispatch(fetchExapleTasks());
-    }; */
+    };
 
-    if (loading) {
-        return (
-          <Button
-            disabled={true}
-            onClick={() => dispatch(fetchExapleTasks())}
-          >
-            Ładowanie...
-          </Button>
-        );
-      }
-    
-      return (
-        <Button onClick={() => dispatch(fetchExapleTasks())}>
-          Pobierz przykładowe zadania
-        </Button>
-      );
 
-    /* if (loading) {
-        return (
-           <>
-                <Button
-                    onClick={openModal}
-                    disabled={loading}
-                >
-                    {loading ? "Ładowanie..." : "Pobierz przykładowe zadania"}
-                </Button>
-                <ModalForm
-                    isOpen={modalIsOpen}
-                >
-                    <h1>Uwaga!</h1>
-                    <ModalText>Ta czynność spowoduje usunięcie wszystich obecnych zadań i zastąpienie ich przykładowaymi.<br /> Czy napewno chcesz kontynuować?</ModalText>
-                    <ModalButtonsGrid>
-                        <ModalButton
-                            onClick={loadingTasks}
-                        >
-                            Tak
-                        </ModalButton>
-                        <ModalButton onClick={closeModal}>Nie</ModalButton>
-                    </ModalButtonsGrid>
-                </ModalForm>
-            </>
-        )
-    }*/
-}
 
+    return (
+        <>
+            <Button
+                onClick={openModal}
+            >
+                {loading ? "Ładowanie..." : "Pobierz przykładowe zadania"}
+            </Button>
+            <ModalForm
+                isOpen={modalIsOpen}
+            >
+                <h1>Uwaga!</h1>
+                <ModalText>Ta czynność spowoduje usunięcie wszystich obecnych zadań i zastąpienie ich przykładowaymi.<br /> Czy napewno chcesz kontynuować?</ModalText>
+                <ModalButtonsGrid>
+                    <ModalButton
+                        onClick={loadingTasks}
+                    >
+                        Tak
+                    </ModalButton>
+                    <ModalButton onClick={closeModal}>Nie</ModalButton>
+                </ModalButtonsGrid>
+            </ModalForm>
+        </>
+    );
+};
 export default ExampleTasksButton;
